e4d16cb2d2e73a820ffba1bd91fed9ad65110afa
Oliver Freudrich <info@oliverfreudrich.com>
2020-09-12 17:13:49 +0200

    + added more default options

--- config/redis.ts
+++ config/redis.ts
@@ -9,5 +9,9 @@ export default class RedisConfiguration extends Configuration {
     default: RedisOptions = {
         port: parseInt(process.env.REDIS_PORT || '6379', 10),
         host: process.env.REDIS_HOST || '0.0.0.0',
+        family: parseInt(process.env.REDIS_FAMILY || '4', 10),
+        password: process.env.REDIS_PASSWORD,
+        db: parseInt(process.env.REDIS_DB || '0', 10),
+        enableReadyCheck: true,
     };
 }
